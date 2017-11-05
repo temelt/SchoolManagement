548fdd33e3a191ae745b3306775acaa25c94f661
vektorel <vektorel@lab3-ogr>
2017-11-05 13:35:46 +0300

    Page added

---
+++ SchoolManagement/src/main/java/com/temelt/schmgt/web/controller/KisiController.java
@@ -0,0 +1,42 @@
+package com.temelt.schmgt.web.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.temelt.schmgt.web.data.repository.KisiRepository;
+import com.temelt.schmgt.web.entity.Kisi;
+
+@Controller("kisiController")
+@Scope("session")
+public class KisiController implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+	private KisiRepository kisiRepository;
+
+	private List<Kisi> kisiList;
+
+	@PostConstruct
+	private void init() {
+		System.out.println("KisiController oluþtu");
+		kisiList = kisiRepository.findAll();
+	}
+
+	public List<Kisi> getKisiList() {
+		return kisiList;
+	}
+
+	public void setKisiList(List<Kisi> kisiList) {
+		this.kisiList = kisiList;
+	}
+}
--- SchoolManagement/src/main/java/com/temelt/schmgt/web/data/repository/KisiRepository.java
+++ SchoolManagement/src/main/java/com/temelt/schmgt/web/data/repository/KisiRepository.java
@@ -2,10 +2,12 @@ package com.temelt.schmgt.web.data.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.temelt.schmgt.web.entity.Kisi;
 
 @Repository
-public interface KisiRepository extends JpaRepository<Kisi, Long>{
+@Transactional
+public interface KisiRepository extends JpaRepository<Kisi, Long> {
 
 }
