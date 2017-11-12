78685e4dc815a5461099d3fa2563c2080f59a4d5
vektorel <vektorel@lab3-8>
2017-11-12 11:40:16 +0200

    ekleme

---
+++ SchoolManagement/src/main/java/com/temelt/schmgt/web/controller/YonetimSalonController.java
@@ -0,0 +1,64 @@
+package com.temelt.schmgt.web.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.temelt.schmgt.web.data.repository.SalonRepository;
+import com.temelt.schmgt.web.entity.kullanici.Kullanici;
+import com.temelt.schmgt.web.entity.yonetim.Salon;
+
+@Controller("SalonController")
+@Scope("session")
+public class YonetimSalonController implements Serializable{
+
+	
+	@Autowired
+	private SalonRepository salonRepository;
+	
+	private Salon salon;
+	private List<Salon> salonList;
+	
+	@PostConstruct
+	private void init() {
+		salonList = salonRepository.findAll();
+		salon=new Salon();
+	}
+	
+	
+	public void kullaniciKaydet() {
+		salonRepository.save(salon);
+		salon=new Salon();
+		salonList = salonRepository.findAll();
+	}
+	
+	public void kullaniciSil(Long id) {
+		Salon s = salonRepository.findOne(id);
+		salonRepository.delete(s);
+		salonList = salonRepository.findAll();
+	}
+	
+	
+	public List<Salon> getSalonList() {
+		return salonList;
+	}
+	
+	public void setSalonList(List<Salon> salonList) {
+		this.salonList = salonList;
+	}
+	
+	public Salon getSalon() {
+		return salon;
+	}
+	
+	public void setSalon(Salon salon) {
+		this.salon = salon;
+	}
+	
+	
+}
