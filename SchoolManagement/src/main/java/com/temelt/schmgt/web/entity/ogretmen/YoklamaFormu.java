8ede55f46bc7438cd7ddf0331d5d291d1648ddae
vektorel <vektorel@lab3-ogr>
2017-11-12 10:08:23 +0300

    Attendence Improv.

--- SchoolManagement/src/main/java/com/temelt/schmgt/web/entity/ogretmen/YoklamaFormu.java
+++ SchoolManagement/src/main/java/com/temelt/schmgt/web/entity/ogretmen/YoklamaFormu.java
@@ -17,6 +17,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import com.temelt.schmgt.web.entity.BaseEntity;
 import com.temelt.schmgt.web.entity.yonetim.Grup;
@@ -27,85 +28,83 @@ import com.temelt.schmgt.web.entity.yonetim.Grup;
  */
 @Entity
 @Table(name = "attendance")
-public class YoklamaFormu extends BaseEntity{
+public class YoklamaFormu extends BaseEntity {
 
-    private Long id;
-    private String yoklamaAciklama;
-    private String islenenKonu;
-    private Grup grup;
-    private YoklamaBilgileri yoklamaBilgileri;
-    private Date yoklamaTarihi;
-    private Date dersTarihi;
+	private Long id;
+	private String yoklamaAciklama;
+	private String islenenKonu;
+	private Grup grup;
+	private YoklamaBilgileri yoklamaBilgileri;
+	private Date yoklamaTarihi;
+	private Date dersTarihi;
 
-    @Id
-    @SequenceGenerator(name = "seq_attendance", allocationSize = 1, sequenceName = "seq_attendance")
-    @GeneratedValue(generator = "seq_attendance", strategy = GenerationType.SEQUENCE)
-    public Long getId() {
-        return id;
-    }
+	@Id
+	@SequenceGenerator(name = "seq_attendance", allocationSize = 1, sequenceName = "seq_attendance")
+	@GeneratedValue(generator = "seq_attendance", strategy = GenerationType.SEQUENCE)
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    @ManyToOne
-    @JoinColumn(name = "group_id")
-    public Grup getGrup() {
-        return grup;
-    }
+	@ManyToOne
+	@JoinColumn(name = "group_id")
+	public Grup getGrup() {
+		return grup;
+	}
 
-    public void setGrup(Grup grup) {
-        this.grup = grup;
-    }
+	public void setGrup(Grup grup) {
+		this.grup = grup;
+	}
 
-    @Column(name = "inspection_description", length = 100)
-    public String getYoklamaAciklama() {
-        return yoklamaAciklama;
-    }
+	@Column(name = "inspection_description", length = 100)
+	public String getYoklamaAciklama() {
+		return yoklamaAciklama;
+	}
 
-    public void setYoklamaAciklama(String yoklamaAciklama) {
-        this.yoklamaAciklama = yoklamaAciklama;
-    }
+	public void setYoklamaAciklama(String yoklamaAciklama) {
+		this.yoklamaAciklama = yoklamaAciklama;
+	}
 
-    @Column(name = "lesson=subject", length = 100)
-    public String getIslenenKonu() {
-        return islenenKonu;
-    }
+	@Column(name = "lesson_subject", length = 100)
+	public String getIslenenKonu() {
+		return islenenKonu;
+	}
 
-    public void setIslenenKonu(String islenenKonu) {
-        this.islenenKonu = islenenKonu;
-    }
+	public void setIslenenKonu(String islenenKonu) {
+		this.islenenKonu = islenenKonu;
+	}
 
-    @ManyToOne
-    @JoinColumn(name = "inspection_info_id")
-    public YoklamaBilgileri getYoklamaBilgileri() {
-        return yoklamaBilgileri;
-    }
+	@ManyToOne
+	@JoinColumn(name = "inspection_info_id")
+	public YoklamaBilgileri getYoklamaBilgileri() {
+		return yoklamaBilgileri;
+	}
 
-    public void setYoklamaBilgileri(YoklamaBilgileri yoklamaBilgileri) {
-        this.yoklamaBilgileri = yoklamaBilgileri;
-    }
+	public void setYoklamaBilgileri(YoklamaBilgileri yoklamaBilgileri) {
+		this.yoklamaBilgileri = yoklamaBilgileri;
+	}
 
-    @Column(name = "inspection_date")
-    @Temporal(javax.persistence.TemporalType.DATE)
-    public Date getYoklamaTarihi() {
-        return yoklamaTarihi;
-    }
+	@Column(name = "inspection_date")
+	@Temporal(TemporalType.DATE)
+	public Date getYoklamaTarihi() {
+		return yoklamaTarihi;
+	}
 
-    public void setYoklamaTarihi(Date yoklamaTarihi) {
-        this.yoklamaTarihi = yoklamaTarihi;
-    }
+	public void setYoklamaTarihi(Date yoklamaTarihi) {
+		this.yoklamaTarihi = yoklamaTarihi;
+	}
 
-    @Column(name = "course_date")
-    @Temporal(javax.persistence.TemporalType.DATE)
-    public Date getDersTarihi() {
-        return dersTarihi;
-    }
+	@Column(name = "course_date")
+	@Temporal(TemporalType.DATE)
+	public Date getDersTarihi() {
+		return dersTarihi;
+	}
 
-    public void setDersTarihi(Date dersTarihi) {
-        this.dersTarihi = dersTarihi;
-    }
-
-    
+	public void setDersTarihi(Date dersTarihi) {
+		this.dersTarihi = dersTarihi;
+	}
 
 }
