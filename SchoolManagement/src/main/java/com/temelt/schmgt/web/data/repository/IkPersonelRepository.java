11d206623218f21fda41a8168310540fc824aeb0
vektorel <vektorel@lab5-3>
2017-12-03 11:58:42 +0300

    İK personel,personel converter,repository

--- SchoolManagement/src/main/java/com/temelt/schmgt/web/data/repository/IkPersonelRepository.java
+++ SchoolManagement/src/main/java/com/temelt/schmgt/web/data/repository/IkPersonelRepository.java
@@ -1,12 +1,17 @@
 package com.temelt.schmgt.web.data.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
 
 import com.temelt.schmgt.web.entity.ik.Personel;
 @Repository
 
 public interface IkPersonelRepository extends JpaRepository<Personel , Long>  {
-
+	@Query(value="select * from teacher where name ilike %?1% or surname ilike %?1%",nativeQuery=true)
+	List<Personel> getByAd(String query);
 }
